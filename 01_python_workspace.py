f82ab41abe224535c0d3aab65a548446fc1287c4
Harshvardhan <harshvardhanvipat@gmail.com>
2020-05-23 01:49:01 +1000

    made some changes to this file

--- 01_python_workspace.py
+++ 01_python_workspace.py
@@ -20,3 +20,21 @@ None # like the idea of 0 -> nothing
 #math functions 
 # data types 
 print(round(23.23423, 2))
+
+#operator precedence
+print((20-3) + 2**2)
+print(bin(9))
+
+# result of this operation would be 21
+
+# string testing start here 
+tet = "Hi this is a string "
+print(tet)
+#  string concatenation 
+print('Hello' + ' ' + 'world' ) # this is an example of string concatenation 
+
+# formatted strings 
+age = 55
+name = 'test'
+print(f'hi {name} you are {age} years old') # python 3 new feature
+print("Hi your name is {} and your age is {}".format(name, age))
