7dc82fb3cc078c32c3530c6aaaef6c6920f46dae
Harshvardhan <harshvardhanvipat@gmail.com>
2020-05-22 21:46:51 +1000

    some more trials of basics

--- 01_python_workspace.py
+++ 01_python_workspace.py
@@ -1,16 +1,22 @@
 print ('harshvardhan')
 #fundamental data types 
+
+# we are talking about floating point number
 int
 float
-str
-list 
-tuple
-set
-dict
-bool
 
+
+# rint(type(6))
+# print (type(2-3))
+# print(type(2/4))
+# print(type(20+1.1))
+print(2**3)
 #classes -> custom data types
 
 #specialized data types 
 # modules -> extensions 
 None # like the idea of 0 -> nothing 
+
+#math functions 
+# data types 
+print(round(23.23423, 2))
