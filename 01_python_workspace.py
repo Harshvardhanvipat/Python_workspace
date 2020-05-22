3106bd963f900f6f74c6690b04da7ad9e2aa2b08
Harshvardhan <harshvardhanvipat@gmail.com>
2020-05-22 21:01:02 +1000

    datatypes

--- 01_python_workspace.py
+++ 01_python_workspace.py
@@ -1,1 +1,16 @@
 print ('harshvardhan')
+#fundamental data types 
+int
+float
+str
+list 
+tuple
+set
+dict
+bool
+
+#classes -> custom data types
+
+#specialized data types 
+# modules -> extensions 
+None # like the idea of 0 -> nothing 
