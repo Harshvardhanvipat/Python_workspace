5d1d61463c97a672d3cd85efec5facce3d48f08e
Harshvardhan <harshvardhanvipat@gmail.com>
2020-05-24 04:34:44 +1000

    debugging files using f5 on vs code / installed pep8 -indentation of code/ linting that is used to check format of the file

--- basic_practice/dictionary_practice.py
+++ basic_practice/dictionary_practice.py
@@ -8,7 +8,7 @@ if is_old:
 else:
     print('You are young please come back later')
 
-#ternary Operator
+# ternary Operator
 
 is_friend = False
 can_message = "Message allowed" if is_friend else "not allowed to message"
@@ -21,9 +21,9 @@ is_test = True
 if is_australia_a_country and is_test:
     print("Australia is a country")
 
-# || && these are used for short curcuiting as 
+# || && these are used for short curcuiting as
 
-# Logical operators 
+# Logical operators
 
 # print(4>5)
 # print(4<5)
@@ -38,7 +38,7 @@ is_magician = True
 is_expert = True
 
 # check if magician and expert : "Print you are an master magician"
-# 
+#
 
 # if is_magician and is_expert:
 #     print("You are a master magician")
@@ -50,12 +50,12 @@ is_expert = True
 # elif is_magician == False:
 #     print("You need magical power")
 
- # == checks for equality 
-print(True == 1) #true
-print(""==1) #false
-print([] ==1) #false
-print(10 == 10.0) #true
-print([] == []) # true 
+# == checks for equality
+print(True == 1)  # true
+print("" == 1)  # false
+print([] == 1)  # false
+print(10 == 10.0)  # true
+print([] == [])  # true
 
 # iterables
 
@@ -74,7 +74,7 @@ print([] == []) # true
 # for item in user.keys():
 #     print(item)
 
-#counter 
+# counter
 
 # my_list = [1,2,3,4,5,6,7,8,9,10]
 # total_of_list = 0
@@ -82,7 +82,7 @@ print([] == []) # true
 #     total_of_list += item
 # print(total_of_list)
 
-# range function 
+# range function
 
 # picture = [
 #     [0,0,0,0,0,0,1,0,0,0],
@@ -101,8 +101,8 @@ print([] == []) # true
 #             print("*",end=" ")
 #     print("\n")
 
-# find the duplicates 
-some_list = ['a','b','c','d','e','d','m','n','m']
+# find the duplicates
+some_list = ['a', 'b', 'c', 'd', 'e', 'd', 'm', 'n', 'm']
 
 duplicates = []
 for unit in some_list:
@@ -112,7 +112,35 @@ for unit in some_list:
 
 print(duplicates)
 
+
 def say_hello():
     print('Hello ! what are you doing ??')
 
+
 say_hello()
+
+# parameters are the used when you define a function
+
+
+def add_numbers(value1, value2):
+    value3 = value1 + value2
+    print(value3)
+
+
+# here 2 and 3 are arguments
+# parameters are used when functions are defined
+# arguments are used when a function is called
+add_numbers(2, 3)
+
+
+def checkDriverAge(age):
+    if int(age) > 18:
+        print("Powering On. Enjoy the ride!")
+    elif int(age) < 18:
+        print("Sorry, you are too young to drive this car")
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride")
+
+
+print('hello')
+checkDriverAge(21)
