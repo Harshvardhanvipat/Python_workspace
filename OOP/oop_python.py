012dc75662948e0e7d00d978c0ee97bb3d045abe
Harshvardhan <harshvardhanvipat@gmail.com>
2020-05-24 06:12:41 +1000

    basic object created

---
+++ OOP/oop_python.py
@@ -0,0 +1,14 @@
+# object oriented programming
+# organized code
+# structured code and modularized code
+# class name are Camel Case
+
+class BigObject:  # Class
+    pass
+
+
+obj1 = BigObject()  # instanciate
+
+# created a object of BigObject
+print(type(obj1))
+# checking the class type of the object
