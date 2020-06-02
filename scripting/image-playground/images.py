5be6f7ebd38e6e7be0920d4315d5b8d0de712b47
Harshvardhan <harshvardhanvipat@gmail.com>
2020-06-03 02:37:40 +1000

    other of some functionality and methods in PILLOW

--- scripting/image-playground/images.py
+++ scripting/image-playground/images.py
@@ -3,8 +3,11 @@ from PIL import Image, ImageFilter
 img = Image.open('./pokedox/image1.jpg')
 # filteredImage = img.filter(ImageFilter.SMOOTH)
 filtered_image = img.convert('L')
-filtered_image.save("blur.png", 'png')
-
+filtered_image.save("grey.png", 'png')
+filtered_image.show()
+filtered_image.resize((200, 200))
+rotated_image = filtered_image.rotate(90)
+rotated_image.show()
 
 # print(img.format)
 # print(img.size)
