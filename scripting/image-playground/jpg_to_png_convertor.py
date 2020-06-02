a4bf87b66fad2485d16faa59bb8549b00cdb3525
Harshvardhan <harshvardhanvipat@gmail.com>
2020-06-03 03:44:21 +1000

    not working there is some issue resolving the path of the input directory

--- scripting/image-playground/jpg_to_png_convertor.py
+++ scripting/image-playground/jpg_to_png_convertor.py
@@ -1,6 +1,3 @@
-from PIL import Image, ImageFilter
-import sys
-import os
 
 # grab the first and the second argument
 
@@ -11,16 +8,33 @@ import os
 # folder_new = '  '
 
 
-input_folder = sys.argv[1]
-output_folder = sys.argv[2]
+# input_folder = sys.argv[1]
+# output_folder = sys.argv[2]
 
-print(input_folder, output_folder)
+# print(input_folder, output_folder)
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+# if not os.path.exists(output_folder):
+#     os.makedirs(output_folder)
 
-for filename in os.listdir(input_folder):
-    print(input_folder)
-    img = Image.open(f"./{input_folder}{filename}", "rb")
-    img.save(f'{output_folder}{filename}', 'png')
-    print("all_done")
+import sys
+import os
+from PIL import Image
+
+path = sys.argv[1]
+directory = sys.argv[2]
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+for filename in os.listdir(path):
+    clean_name = os.path.splitext(filename)[0]
+    img = Image.open(f'{path}{filename}')
+    # added the / in case user doesn't enter it. You may want to check for this and add or remover it.
+    img.save(f'{directory}/{clean_name}.png', 'png')
+    print('all done!')
+
+    # print(input_folder)
+    # img = Image.open(f"./{input_folder}{filename}", "rb")
+    # img.save(f'{output_folder}{filename}', 'png')
+    # print("all_done")
